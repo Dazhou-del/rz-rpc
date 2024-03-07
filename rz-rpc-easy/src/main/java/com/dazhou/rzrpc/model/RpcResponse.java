80ab8336c31fd610ec553c902975fb5d26421a6c
Dazhou-del <1797050390@qq.com>
2024-03-07 14:31:10 +0800

    [+] rpc响应参数

---
+++ rz-rpc-easy/src/main/java/com/dazhou/rzrpc/model/RpcResponse.java
@@ -0,0 +1,37 @@
+package com.dazhou.rzrpc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author <a href="https://github.com/Dazhou-del">Dazhou</a>
+ * @create 2024-03-07 14:29
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RpcResponse {
+
+    /**
+     * 响应参数
+     */
+    private Object data;
+
+    /**
+     * 响应参数类型
+     */
+    private Class<?> dataType;
+
+    /**
+     * 响应信息
+     */
+    private String message;
+
+    /**
+     * 异常处理
+     */
+    private Exception exception;
+}
