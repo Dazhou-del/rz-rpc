57e2b1d015b3ce27ff14627e141bfd5534e969f1
Dazhou-del <1797050390@qq.com>
2024-04-01 16:01:20 +0800

    [+] 添加文档说明

--- rz-rpc-core/src/main/java/com/dazhou/rzrpc/core/fault/tolerant/TolerantStrategyKeys.java
+++ rz-rpc-core/src/main/java/com/dazhou/rzrpc/core/fault/tolerant/TolerantStrategyKeys.java
@@ -1,29 +1,29 @@
 package com.dazhou.rzrpc.core.fault.tolerant;
 
-/**
- * 容错策略键名常量
- *
- * @author <a href="https://github.com/Dazhou-del">Dazhou</a>
- * @create 2024-03-31 20:56
- */
-public interface TolerantStrategyKeys {
     /**
-     * 故障恢复
+     * 容错策略键名常量
+     *
+     * @author <a href="https://github.com/Dazhou-del">Dazhou</a>
+     * @create 2024-03-31 20:56
      */
-    String FAIL_BACK = "failBack";
+    public interface TolerantStrategyKeys {
+        /**
+         * 故障恢复
+         */
+        String FAIL_BACK = "failBack";
 
-    /**
-     * 快速失败
-     */
-    String FAIL_FAST = "failFast";
+        /**
+         * 快速失败
+         */
+        String FAIL_FAST = "failFast";
 
-    /**
-     * 故障转移
-     */
-    String FAIL_OVER = "failOver";
+        /**
+         * 故障转移
+         */
+        String FAIL_OVER = "failOver";
 
-    /**
-     * 静默处理
-     */
-    String FAIL_SAFE = "failSafe";
-}
+        /**
+         * 静默处理
+         */
+        String FAIL_SAFE = "failSafe";
+    }
